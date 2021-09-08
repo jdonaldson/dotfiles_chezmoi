9ab229f57611382157d0ec71d95b0830a777a0a8
Justin Donaldson <jdonaldson@gmail.com>
2021-09-08 08:47:21 -0700

    add plugins

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -4,6 +4,25 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use "lifepillar/vim-solarized8"
+	vim.cmd "set background=dark"
+	vim.cmd "colorscheme solarized8"
+
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
+
+	use 'alker0/chezmoi.vim'
+
+	use 'neovim/nvim-lspconfig'
+
+	use 'Lilja/vim-chezmoi'
+
 end)
