3f7ca4a7c59ab46825598b6a32ba9a47fc62fa27
Justin Donaldson <jdonaldson@gmail.com>
2021-09-07 20:16:58 -0700

    zimfw

--- run_once_before_install-packages.sh
+++ run_once_before_install-packages.sh
@@ -1,12 +1,3 @@
 #!/bin/zsh
 
-PREZTO="${ZDOTDIR:-$HOME}/.zprezto"
-if [ -d "$PREZTO" ]; then
-	echo "$PREZTO already exists, skipping sync"
-else
-	git clone --recursive https://github.com/sorin-ionescu/prezto.git $PREZTO
-	setopt EXTENDED_GLOB
-	for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^(README.md|zshrc)(.N); do
-	  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-	done
-fi
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
