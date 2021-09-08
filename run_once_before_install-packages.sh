a6c356626e99cdf958b150102867066b6a9abc15
Justin Donaldson <jdonaldson@gmail.com>
2021-09-07 19:17:36 -0700

    add zprezto

---
+++ run_once_before_install-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+PREZTO="${ZDOTDIR:-$HOME}/.zprezto"
+if [ -d "$PREZTO" ]; then
+	echo "$PREZTO already exists, skipping sync"
+else
+	git clone --recursive https://github.com/sorin-ionescu/prezto.git $PREZTO
+	setopt EXTENDED_GLOB
+	for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+	  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+	done
+fi
