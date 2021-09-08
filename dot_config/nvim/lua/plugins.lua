92f6f53cc6edd8ecef787cd5aad8b9ce8f7c922a
Justin Donaldson <jdonaldson@gmail.com>
2021-09-08 11:01:37 -0700

    more chezmoi config

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -6,32 +6,30 @@ return require('packer').startup(function()
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
+
     use {
         'nvim-telescope/telescope.nvim',
         requires = {{'nvim-lua/plenary.nvim'}}
     }
 
-    use { 
-        "lifepillar/vim-solarized8",
-        config =  vim.cmd [[
-            "set background=dark",
-            "colorscheme solarized8"
-        ]]
-    }
+    use  "lifepillar/vim-solarized8"
 
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
 
-
     use 'neovim/nvim-lspconfig'
 
     use {
         'hoob3rt/lualine.nvim',
         requires = {'kyazdani42/nvim-web-devicons', opt = true},
-        config=function() require"lualine".setup{} end
     }
+        require"lualine".setup()
+
+    use 'airblade/vim-gitgutter'
+    use 'tpope/vim-fugitive'
+    use 'tpope/vim-commentary'
 
 end)
 
