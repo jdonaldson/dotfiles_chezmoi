8e255b359a3a05638ce7a93e142267798bed02cb
Justin Donaldson <jdonaldson@gmail.com>
2021-09-07 21:14:27 -0700

    install packages

--- run_once_before_install-packages.sh
+++ run_once_before_install-packages.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
-curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+eval curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+
+eval git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
